e40352c5ec7f34f8ac942c76991fd7c2eb10a013
itsrival <76696160+itsrival@users.noreply.github.com>
2022-06-18 22:59:38 +0700

    Add files via upload

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,1 +1,82 @@
-var _0x20ee8a=_0x4d8d;(function(_0x5f0216,_0xbadb78){var _0x52fbb9=_0x4d8d,_0x2c8a3b=_0x5f0216();while(!![]){try{var _0x3c70c5=-parseInt(_0x52fbb9(0xda))/0x1*(parseInt(_0x52fbb9(0xde))/0x2)+-parseInt(_0x52fbb9(0xc9))/0x3*(-parseInt(_0x52fbb9(0xca))/0x4)+-parseInt(_0x52fbb9(0xdf))/0x5+parseInt(_0x52fbb9(0xe2))/0x6*(-parseInt(_0x52fbb9(0xe0))/0x7)+parseInt(_0x52fbb9(0xd6))/0x8*(parseInt(_0x52fbb9(0xd5))/0x9)+parseInt(_0x52fbb9(0xcd))/0xa+-parseInt(_0x52fbb9(0xce))/0xb*(parseInt(_0x52fbb9(0xd2))/0xc);if(_0x3c70c5===_0xbadb78)break;else _0x2c8a3b['push'](_0x2c8a3b['shift']());}catch(_0x1cfe6b){_0x2c8a3b['push'](_0x2c8a3b['shift']());}}}(_0x588b,0x52055));function _0x4d8d(_0x16adb0,_0x30fd48){var _0x588bfd=_0x588b();return _0x4d8d=function(_0x4d8d06,_0x11472c){_0x4d8d06=_0x4d8d06-0xc7;var _0x5aac6e=_0x588bfd[_0x4d8d06];return _0x5aac6e;},_0x4d8d(_0x16adb0,_0x30fd48);}function _0x588b(){var _0x20de0f=['1087746SVCjMA','getElementById','<br/>\x20Project:\x20','Your\x20Message\x20has\x20been\x20send\x20to\x20developer','48XIQrxO','btn','value','2841471LuWeek','16YTHHXm','Name:\x20','send','success','1AkgBZm','essovbdehctnicox','project','then','708826uYpoDR','1175085roiozr','44359xUXKVD','addEventListener','54mopMlx','fire','preventDefault','toukochan08@gmail.com','807UJCSLx','1140SiIgmc','name','Thanks\x20for\x20submit','6698610ZwfzEd'];_0x588b=function(){return _0x20de0f;};return _0x588b();}var btn=document[_0x20ee8a(0xcf)](_0x20ee8a(0xd3));btn[_0x20ee8a(0xe1)]('click',function(_0x578f95){var _0xfd2350=_0x20ee8a;_0x578f95[_0xfd2350(0xc7)]();var _0x24d771=document[_0xfd2350(0xcf)](_0xfd2350(0xcb))['value'],_0x3b7e37=document[_0xfd2350(0xcf)]('email')[_0xfd2350(0xd4)],_0x2acc25=document[_0xfd2350(0xcf)](_0xfd2350(0xdc))['value'],_0x2fa6e2=_0xfd2350(0xd7)+_0x24d771+'<br/>\x20Email:\x20'+_0x3b7e37+_0xfd2350(0xd0)+_0x2acc25;Email[_0xfd2350(0xd8)]({'Host':'smtp.gmail.com','Username':_0xfd2350(0xc8),'Password':_0xfd2350(0xdb),'To':_0xfd2350(0xc8),'From':_0x3b7e37,'Subject':_0x2acc25,'Body':_0x2fa6e2})[_0xfd2350(0xdd)](_0x361ea5=>Swal[_0xfd2350(0xe3)](_0xfd2350(0xcc),_0xfd2350(0xd1),_0xfd2350(0xd9)));});
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+
+
+var jsonParser = bodyParser.json()
+
+app.use(cors({origin: '*'}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/send-email', jsonParser, async (req, res) => {
+
+    const { name, lastName, email, phone, address, services, phoneCheck, emailCheck, message, correo, logo } = req.body;
+
+    contentHTML = `
+        <div style="
+            width: 70%;
+            margin: 0 auto;
+            background-color: rgb(233, 233, 233);
+            border: 1px solid rgb(208, 208, 208);
+            box-shadow: inset 0px 0px 0px 0px rgba(0, 0, 0, 0.2);
+            padding: 30px;
+        ">
+            <div style="text-align: center;" >
+                <img
+                    src="${logo}"
+                    alt="logo-empresa"
+                    style="width: 30%;"
+                >
+            </div>
+            <h1 style="text-align: center;">Website Formulario</h1>
+            <ul style="list-style: none; padding: 10px;">
+                <li style=" padding: 10px;border-bottom: 1px solid rgb(208, 208, 208);">Name: ${name} ${lastName}</li>
+                <li style=" padding: 10px;border-bottom: 1px solid rgb(208, 208, 208);">Email: ${email}</li>
+                <li style=" padding: 10px;border-bottom: 1px solid rgb(208, 208, 208);">Phone: ${phone}</li>
+                <li style=" padding: 10px;border-bottom: 1px solid rgb(208, 208, 208);">Address: ${address}</li>
+                <li style=" padding: 10px;border-bottom: 1px solid rgb(208, 208, 208);">Services: ${services}</li>
+                <li style=" padding: 10px;border-bottom: 1px solid rgb(208, 208, 208);">Best Way To Contact You: <br>
+                ${phoneCheck}, ${emailCheck}</li>
+            </ul>
+            <p style="padding: 20px;border: 1px solid rgb(208, 208, 208);">${message}</p>
+        </div>
+    `;
+
+    const transporter = nodemailer.createTransport({
+        host: 'mail.lalemanllc.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'admin@zynfx.site',
+            pass: 'dimas2005'
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    const info = await transporter.sendMail({
+        from: `"${name}" <test@lalemanllc.com>`, // sender address,
+        to: `${correo}`,
+        subject:`${email}`,
+        html: contentHTML
+    })
+
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // // Preview only available when sending through an Ethereal account
+    // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    res.json({ message: 'Email has been sent', success: true });
+})
+
+const port = process.env.PORT || 5000;
+console.log({ port });
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
